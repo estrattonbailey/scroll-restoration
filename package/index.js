6b6b26bb0836d6a9160365964d4abd20d209143e
estrattonbailey <ericstrattonbailey@gmail.com>
2017-01-28 12:17:41 -0500

    Fix undefined props

--- package/index.js
+++ package/index.js
@@ -1,18 +1,20 @@
 let time = 0
 let prevtime = 0
 let distance = 0
-let prevscroll = 0
+let prevscroll = null
 let pool = []
 
-export default (y, { timeStamp: t }, config = {}) => {
+export default (y, event = { timeStamp: 0 }, config = {}) => {
   let total = 0
 
-  time = t - prevtime
-  distance = Math.abs(y - prevscroll)
-  prevscroll = y
-  prevtime = t
+  if (!event.timeStamp || event.timeStamp === 0) return 0
 
-  pool.push(distance/(time * (1 / config.interval || 100)))
+  time = event.timeStamp - prevtime
+  distance = Math.abs(y - prevscroll || 0)
+  prevscroll = y
+  prevtime = event.timeStamp
+
+  pool.push(distance/(time * (1 / (config.interval || 100))))
   if (pool.length > (config.pool || 10)) pool.shift()
 
   for (let i = 0; i < pool.length; i++) {
